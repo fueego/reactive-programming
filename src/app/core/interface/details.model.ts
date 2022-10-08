4589df532461a369c3ae9a7f1e8a0accbcd00e63
Kuba <programistyczne.studio@gmail.com>
2022-10-08 20:53:03 +0200

    remove not needed interface

--- src/app/core/interface/details.model.ts
+++
@@ -1,8 +0,0 @@
-import { Category } from 'src/app/core/interface/category.model';
-import { LinkItemData } from 'src/app/core/interface/link.model';
-
-export interface DetailsItemData
-  extends Pick<Category, 'categoryId'>,
-    Pick<LinkItemData, 'linkId' | 'url' | 'shortDescription'> {
-  notes: string;
-}
